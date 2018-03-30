54d83fc9fa8fa6ba0c9219e8f8507e81e728bc9d
tomsaze <josetomasazevedo@hotmail.com>
2018-03-30 19:16:53 +0100

    tcp example

--- AER/src/aer/TCP/ListenerTCP.java
+++ AER/src/aer/TCP/ListenerTCP.java
@@ -5,7 +5,17 @@
  */
 package aer.TCP;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,14 +23,51 @@ import java.net.SocketException;
  */
 public class ListenerTCP implements Runnable{
 
+    ServerSocket s1;
+    Socket ss;
+    Scanner sc;
+    PrintStream p;
     public ListenerTCP() throws SocketException {
        
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        try {
+       /* int number, temp;
+        s1 = new ServerSocket(9999);
+        ss = s1.accept();
+        sc = new Scanner(ss.getInputStream());
+        number = sc.nextInt();
+        
+        temp  = number *2;
+        
+        p = new PrintStream(ss.getOutputStream());
+        p.println(temp);*/
+        
+        
+        String clientSentence;
+        String capitalizedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+         while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+  }
+        } catch (IOException ex) {
+            Logger.getLogger(ListenerTCP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+      
+// throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
